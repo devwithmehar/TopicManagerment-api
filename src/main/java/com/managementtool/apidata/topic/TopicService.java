d995e37d1853dd088319d7d1814823ee2a3aace6
devwithmehar <meharsahil207@gmail.com>
2023-11-29 00:10:13 -0600

    data is saved in derby database

--- src/main/java/com/managementtool/apidata/topic/TopicService.java
+++ src/main/java/com/managementtool/apidata/topic/TopicService.java
@@ -3,20 +3,27 @@ package com.managementtool.apidata.topic;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class TopicService {
+public class TopicService  {
+
+    @Autowired
+    private TopicRepository topicRepository;
     
     private List<Topic> topics = new ArrayList<>( Arrays.asList(
-        new Topic( "Lean Java", "For DSA, for leaning high scalable language"),
-        new Topic( "Spring Framework", "Helps me to build application for Desktop, Web, Microservices"),
-        new Topic( "React", "React is the javascript Libarary")
+        new Topic( "1", "Lean Java", "For DSA, for leaning high scalable language"),
+        new Topic( "2","Spring Framework", "Helps me to build application for Desktop, Web, Microservices"),
+        new Topic("3", "React", "React is the javascript Libarary")
     ));
 
     public List<Topic> getAllTopics(){
-        return this.topics;
+        List<Topic> topics = new ArrayList<>();
+        topicRepository.findAll().forEach(topics:: add);
+        return topics;
     }
 
     // Time Complexity will be O(n)
@@ -26,14 +33,13 @@ public class TopicService {
 
 
     public void addTopic(Topic topic){
-        topics.add(topic);
+        topicRepository.save(topic);
     }
 
     public void updateTopic(String id, Topic topic){
        for(int i = 0; i < topics.size(); i++){
          Topic t = topics.get(i);
          if(t.getId().equals(id)){
-            topic.setId(id);
             topics.set(i,topic);
             return;
          }
