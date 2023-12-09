708311df3aac8f4b03ce2717e8e14ab9d923143f
devwithmehar <meharsahil207@gmail.com>
2023-12-09 04:23:56 -0600

    API Projected completed

--- src/main/java/com/managementtool/apidata/course/CourseController.java
+++ src/main/java/com/managementtool/apidata/course/CourseController.java
@@ -1,21 +1,24 @@
 package com.managementtool.apidata.course;
 
 import java.util.List;
+import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.managementtool.apidata.topic.Topic;
 
+@RestController
 public class CourseController {
     
     @Autowired
     private CourseService courseService;
 
-    @RequestMapping("topics/{topicId}/courses")
+    @RequestMapping("/topics/{topicId}/courses")
     public List<Course> getAllCourses(@PathVariable String topicId){
         return courseService.getAllCourses(topicId);
     }
--- src/main/java/com/managementtool/apidata/course/CourseService.java
+++ src/main/java/com/managementtool/apidata/course/CourseService.java
@@ -5,9 +5,10 @@ import java.util.ArrayList;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 
-
+@Service
 public class CourseService {
 
     @Autowired
